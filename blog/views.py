95361f1dc91a882ad1115ff15ffdb1ae47f66029
NaifatSelina <naifatselina@icloud.com>
2024-01-13 20:30:14 +0000

    fix url path so page loads

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Article
 
 class ArticleList(generic.ListView):
     model = Article
-    queryset = Article.objects.filter(status=1).order_by("-created_on")
+    queryset = Article.objects.filter(publication_status=1).order_by("-publication_date")
     template_name = "index.html"
     paginate_by = 6
+
