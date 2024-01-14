cff276c17c453a85f6c1188b44cf1ffa7cca0a33
NaifatSelina <naifatselina@icloud.com>
2024-01-14 20:38:20 +0000

    housekeeping

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://jvxtvqfh:XaPH0mSZm_i_WoHTmw05EwLG7ruI04iW@rogue.db.elephantsql.com/jvxtvqfh"
+os.environ["SECRET_KEY"]="my_super_secretkey"
+os.environ["CLOUDINARY_URL"]="cloudinary://876952333194932:Vjs48HRAOyC_yQjmZLbx2PklC84@dxs8nfjnp"
+
